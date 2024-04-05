688a313ad43c96e1dee93d98e57703b73cfb3ed4
heitorwirmond <heitorfranciscowirmond@hotmail.com>
2024-04-05 00:03:30 -0300

    first branch

---
+++ src/components/ProjectCards.js
@@ -0,0 +1,16 @@
+import { Col } from "react-bootstrap";
+
+function ProjectCards({title, description, imgUrl}){
+    return(
+        <Col sm={6} md={4} >
+            <div className="proj-imbx">
+            <img src={imgUrl}/>
+            <div className="proj-txtx">
+                <h4>{title}</h4>
+                <span>{description}</span>
+            </div>
+            </div>
+        </Col>
+    )
+}
+export default ProjectCards;
