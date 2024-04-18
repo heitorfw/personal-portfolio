d60a13d79d5246698ff07f57571aa860f4a3a2bd
heitorwirmond <57256773+heitorfw@users.noreply.github.com>
2024-04-18 18:59:53 -0300

    Update ProjectCards.js

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -4,7 +4,9 @@ function ProjectCards({title, description, imgUrl}){
     return(
         <Col sm={6} md={4} >
             <div className="proj-imbx">
+            a href={'https://zoom-app-xi.vercel.app'} target="_blank" rel="noopener noreferrer">
             <img src={imgUrl}/>
+            </a>
             <div className="proj-txtx">
                 <h4>{title}</h4>
                 <span>{description}</span>
