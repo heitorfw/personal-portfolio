0fdf56a6619a184d31e0c2ca5c4819e855ac25e1
heitorwirmond <57256773+heitorfw@users.noreply.github.com>
2024-04-18 23:10:40 -0300

    Update ProjectCards.js

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -5,7 +5,7 @@ function ProjectCards({title, description, imgUrl}){
         <Col sm={6} md={4} >
             <div className="proj-imbx">
             <a href={'https://zoom-app-xi.vercel.app'} target="_blank" rel="noopener noreferrer">
-            <img src={imgUrl} className="rounded" style={{ width: '100%', border-radius: '20px' }}/>
+            <img src={imgUrl} className="rounded" style={{ width: '100%', borderRadius: '20px' }}/>
             </a>
             <div className="proj-txtx">
                 <h4>{title}</h4>
