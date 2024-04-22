453449a9e81d104c167df4d74841ac870d0454ba
heitorwirmond <57256773+heitorfw@users.noreply.github.com>
2024-04-22 01:13:40 -0300

    Update ProjectCards.js

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,19 +1,24 @@
 import { Col } from "react-bootstrap";
 
-function ProjectCards({title, description, imgUrl}){
-    return(
-        <Col sm={6} md={4} >
-            <div className="proj-imbx">
-            <a href={'https://zoom-app-xi.vercel.app'} target="_blank" rel="noopener noreferrer">
-            <img src={imgUrl} className="rounded" style={{ width: '110%', borderRadius: '100vh'}}/>
-            </a>
-            <div className="proj-txtx">
-                <h4>{title}</h4>
-                <span>{description}</span>
-
-            </div>
-            </div>
-        </Col>
-    )
+function ProjectCards({ title, description, imgUrl, link }) {
+  
+  return (
+    <Col sm={6} md={4}>
+      <div className="proj-imbx position-relative">
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <img src={imgUrl} className="rounded img-fluid mx-2" style={{ width: '110%', borderRadius: '100vh', whiteSpace:'10px'}}/>
+          <div className="overlay">
+            <br></br>
+            <h4 style={{color: '#fff', opacity: '1'}}>{title}</h4>
+            <p style={{color: '#fff', opacity: '0.8'}}>{description} </p>
+          </div>
+        </a>
+        <div className="proj-txtx">
+          
+        </div>
+      </div>
+    </Col>
+  );
 }
+
 export default ProjectCards;
